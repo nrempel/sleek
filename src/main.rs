97e87355353d3f271c68f6f6af92b4eef12c8bf6
Nicholas Rempel <nick@instant.ca>
2023-04-07 14:20:53 -0700

    Add a test

--- src/main.rs
+++ src/main.rs
@@ -79,3 +79,40 @@ struct Options {
     #[clap(short, long, default_value = "2")]
     lines_between_queries: u8,
 }
+
+#[cfg(test)]
+mod tests {
+    use std::path::Path;
+
+    use super::*;
+
+    #[test]
+    fn test_process_file() -> Result<(), Box<dyn Error>> {
+        let test_input = "SELECT * from users;";
+        let test_output = "SELECT\n    *\nFROM\n    users;";
+        let test_path = "test_input.sql";
+
+        // Create the test input file
+        fs::write(test_path, test_input)?;
+
+        let format_options = FormatOptions {
+            indent: Indent::Spaces(4),
+            uppercase: true,
+            lines_between_queries: 2,
+        };
+
+        // Run the process_file function on the test input file
+        process_file(&Path::new(test_path).to_path_buf(), false, format_options)?;
+
+        // Read the contents of the formatted file
+        let formatted_contents = fs::read_to_string(test_path)?;
+
+        // Clean up the test file
+        fs::remove_file(test_path)?;
+
+        // Assert that the contents match the expected output
+        assert_eq!(formatted_contents, test_output);
+
+        Ok(())
+    }
+}
